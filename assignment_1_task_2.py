81068a2377472b876166966d8b5db158cce12aec
smeetmehta-27 <smeet.chiragmehta2024@vitstudent.ac.in>
2026-02-10 22:56:45 +0530

    Task-1-Commit

---
+++ assignment_1_task_2.py
@@ -0,0 +1,5 @@
+first_name = str(input("Enter your first name: "))
+last_name = str(input("Enter your last name: "))
+full_name = first_name + " " + last_name
+
+print("Hello, "+full_name+"! Welcome to Python program.")
